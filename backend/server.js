16f325795c35040cf1129f2c23c84f5e83be0e96
mahammadaftab <mdaftabeditz360@gmail.com>
2025-08-07 22:31:38 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,3 @@ mongoose.connect(process.env.MONGO_URI, {
     console.log(`Server running on port ${process.env.PORT}`)
   );
 }).catch((err) => console.log(err));
-
-import paymentRoutes from './routes/paymentRoutes.js';
-app.use('/api/payments', paymentRoutes);
